739b5456ad1ea12861cdf0f9db8589a712840367
Huan <huanyang.ooi@gmail.com>
2023-12-18 01:23:39 +0100

    cleaned up

---
+++ Lab2-Threads/Race.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab2_Threads
+{
+    internal class Race
+    {
+        // all times are adjusted according to the scale of 1/10.
+        public static string GetLeader(Car car1, Car car2)
+        {
+            if (car1.DistanceTraveled == car2.DistanceTraveled)
+            {
+                // Compare the times taken to reach the current distance
+                TimeSpan car1Time = car1.TimeLog.Count > 0 ? car1.TimeLog[car1.DistanceTraveled - 1] : TimeSpan.MaxValue;
+                TimeSpan car2Time = car2.TimeLog.Count > 0 ? car2.TimeLog[car2.DistanceTraveled - 1] : TimeSpan.MaxValue;
+
+                return car1Time < car2Time ? car1.Name : car2.Name;
+            }
+
+            return car1.DistanceTraveled > car2.DistanceTraveled ? car1.Name : car2.Name;
+        }
+    }
+}
---
+++ Lab2-Threads/Utility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Threads
+{
+    internal class Utility
+    {
+        internal static void CheckForKeyPress(CancellationToken cancellationToken, Car car1, Car car2, CancellationTokenSource cancellationTokenSource)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Press Enter to check status or Esc to exit.");
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{car1.Name}: {car1.DistanceTraveled} km, Speed: {car1.Speed} km/h");
+                    Console.WriteLine($"{car2.Name}: {car2.DistanceTraveled} km, Speed: {car2.Speed} km/h");
+                    Console.WriteLine($"{Race.GetLeader(car1, car2)} is in the lead!");
+
+                }
+                else if (key.Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("Race has been stopped");
+                    cancellationTokenSource.Cancel();
+                    Console.WriteLine("Thanks for racing!");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Option not recognized");
+                }
+            }
+        }
+    }
+}
